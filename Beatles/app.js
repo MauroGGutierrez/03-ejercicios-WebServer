e428c3f7565322a86dcd5a97da29c983e4ba2a89
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-03-16 18:31:07 -0300

    se termino el showbyname

--- Beatles/app.js
+++ Beatles/app.js
@@ -1,37 +1,27 @@
 var http = require("http");
 var fs = require("fs");
-
-var beatles = [
-  {
-    name: "John Lennon",
-    birthdate: "09/10/1940",
-    profilePic:
-      "https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg",
-  },
-  {
-    name: "Paul McCartney",
-    birthdate: "18/06/1942",
-    profilePic:
-      "http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg",
-  },
-  {
-    name: "George Harrison",
-    birthdate: "25/02/1946",
-    profilePic:
-      "https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg",
-  },
-  {
-    name: "Richard Starkey",
-    birthdate: "07/08/1940",
-    profilePic:
-      "http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg",
-  },
-];
+const beatles = require("./beatles");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    var html = fs.readFileSync(__dirname + "/index.html");
-    res.end(html);
+    if (req.url === "/") {
+      //Si la URL es / devolvemos el HTML
+      res.writeHead(200, { "Content-Type": "text/html" });
+      var html = fs.readFileSync(__dirname + "/index.html");
+      res.end(html);
+    } else if (req.url === "/api") {
+      //Si la URL es /arcoiris_doge devolvemos la imagen
+      res.writeHead(200, { "Content-Type": "text/json" });
+      var api = fs.readFileSync(__dirname + "/beatles.js");
+      res.end(api);
+    } else if (req.url === "/api/JohnLennon") {
+      //Si la URL es /arcoiris_doge devolvemos la imagen
+      res.writeHead(200, { "Content-Type": "text/json" });
+      var btl1 = fs.readFileSync(__dirname + beatles[1]);
+      res.end(btl1);
+    } else {
+      res.writeHead(404); //Ponemos el status del response a 404: Not Found
+      res.end(); //No devolvemos nada más que el estado.
+    }
   })
   .listen(1337, "127.0.0.1");
